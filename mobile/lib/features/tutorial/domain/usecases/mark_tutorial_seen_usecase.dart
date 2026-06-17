f2a8130493bc951adc3e0d6f582af61cdc79ecc3
Jorge Flores <jorgeafa.19@gmail.com>
2026-06-16 20:57:59 -0500

    feat(tutorial): add guided onboarding coach marks for new users
    Implements in-context feature discovery (tutorial_coach_mark v1.3.3)
    shown once after first login on the map screen. Highlights 5 targets:
    search, report FAB, alerts, risk map and panic button. Progress 1/5,
    skip support, and Hive persistence (tutorial_seen in app_prefs box).
    Adds "Ver tutorial de nuevo" option in ProfilePage.

---
+++ mobile/lib/features/tutorial/domain/usecases/mark_tutorial_seen_usecase.dart
@@ -0,0 +1,14 @@
+import 'package:dartz/dartz.dart';
+import 'package:injectable/injectable.dart';
+
+import 'package:alertaya/core/errors/failures.dart';
+import 'package:alertaya/features/tutorial/domain/repositories/tutorial_repository.dart';
+
+@injectable
+class MarkTutorialSeenUseCase {
+  const MarkTutorialSeenUseCase(this._repository);
+
+  final TutorialRepository _repository;
+
+  Future<Either<Failure, Unit>> call() => _repository.markTutorialSeen();
+}
